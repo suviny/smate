c396f9ad3f366f7e7305768527d09a6242c4d715
suviny <tnqls3718@gmail.com>
2025-10-31 02:09:59 +0900

    최초 커밋

---
+++ src/main/java/org/viny/smate/SmateApplication.java
@@ -0,0 +1,13 @@
+package org.viny.smate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/viny/smate/SmateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.viny.smate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
